b52855678df9721fdc6ffb221766453ca7b1ff28
Joshua Reisbord <joshuareisbord@gmail.com>
2020-09-26 17:58:49 -0700

    Moved backend to its own folder and changed wikipedia response to give sumamry

--- backend/response.py
+++ backend/response.py
@@ -7,13 +7,15 @@ import wit
 witClient = wit.Wit("DTIBACFI5NLIEIR7EOAHBGDNKTYXEA2A")
 wolframalphaClient = wolframalpha.Client("QAT4UH-YT4UEQW94X")
 
-def get_page_object_wikipedia(page: str) -> wikipedia.WikipediaPage:
+
+def get_response_wikipedia(page: str) -> str:
 	"""
 	Returns a page object containing all information of a Wikipedia page
-	:param search: String which is a valid wikipedia page
+	:param page: String which is a valid wikipedia page
 	:return: WikipediaPage object
 	"""
-	return wikipedia.search(page)
+	return wikipedia.page(wikipedia.search(page)[0]).summary
+
 
 def get_response_wolframaplha(message: str) -> wolframalpha.Result:
 	"""
@@ -23,6 +25,7 @@ def get_response_wolframaplha(message: str) -> wolframalpha.Result:
 	"""
 	return wolframalphaClient.query(message)
 
+
 def get_response_wit(message: str) -> json:
 	"""
 	Accepts a message in string format and responds with the response in JSON format from Wit.ai
@@ -31,16 +34,13 @@ def get_response_wit(message: str) -> json:
 	"""
 	return witClient.message(message)
 
-if __name__ == '__main__':
-	search_phrase = wikipedia.suggest("powrche 911")
-	search = wikipedia.search(search_phrase)
-	page = wikipedia.page(search[0])
 
-	print(type(page))
-	print(page.summary)
+if __name__ == '__main__':
+	search_phrase = "Porsche 911"
+	response = get_response_wikipedia(search_phrase)
+	print(response)
 
 	import json
-
 	response = get_response_wit("i want to learn about the porsche 911")
-	print(json.dumps(response, indent=4, sort_keys=True))
+	print(json.dumps(response, indent=4))
 
